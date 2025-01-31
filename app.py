a2d8ceb7f47fff5050cfbba4f7bcfee7ddc95be0
Leon Greil <lgreil@uni-muenster.de>
2025-01-31 21:32:15 +0100

    potential fix for code scanning alert no. 2

--- app.py
+++ app.py
@@ -71,4 +71,4 @@ def logout():
 
 if __name__ == '__main__':
     # Run the Flask application
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
